f3097d92ab6d87c73521f2c9f24dcf5e3830fdde
Arbinamansuri <108967486+Arbinamansuri@users.noreply.github.com>
2022-08-04 16:27:11 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,15 @@
+public class Dog extends Animal
+ {
+	final void sound()
+ {
+		System.out.println();
+		System.out.println("Additional Characteristics : ");
+		System.out.println("Sound : Bhow Bhow");
+	}
+	public static void main(String[] args) 
+{
+		Dog d = new Dog();
+		d.characteristics();
+		d.sound();
+	}
+}
